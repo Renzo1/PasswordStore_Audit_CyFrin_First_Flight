76d27370f22f24db1edab444ab0fb4d94ace056c
Renzo1 <krisrenzo79@gmail.com>
2023-10-26 23:12:04 +0100

    first commit

--- script/DeployPasswordStore.s.sol
+++ script/DeployPasswordStore.s.sol
@@ -13,3 +13,17 @@ contract DeployPasswordStore is Script {
         return passwordStore;
     }
 }
+
+// forge script script/DeployPasswordStore.s.sol:DeployPasswordStore --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY --broadcast -vv
+
+// cast storage 0xPasswordStoreContractAddress 1 --rpc-url $SEPOLIA_RPC_URL
+
+// 0x0046aAA6DFCACA10B6f721b356aB870eca5dcd7a
+// cast storage 0x0046aAA6DFCACA10B6f721b356aB870eca5dcd7a 1 --rpc-url $SEPOLIA_RPC_URL
+
+// 0x6d7950617373776f726400000000000000000000000000000000000000000014
+
+// You can read the data by converting it from byte32 to ASCII
+// cast --to-ascii 0x98765ReturnedByte32Data321000000000000000000000000000000000000014
+
+// cast --to-ascii    0x6d7950617373776f726400000000000000000000000000000000000000000014
--- test/PasswordStore.t.sol
+++ test/PasswordStore.t.sol
@@ -31,3 +31,42 @@ contract PasswordStoreTest is Test {
         passwordStore.getPassword();
     }
 }
+
+// My Additional Tests
+
+contract MorePasswordStoreTest is Test {
+    PasswordStore public passwordStore;
+    DeployPasswordStore public deployer;
+    address public owner;
+    address public notOwner;
+
+    function setUp() public {
+        deployer = new DeployPasswordStore();
+        passwordStore = deployer.run();
+        owner = msg.sender;
+        notOwner = address(1);
+        string memory expectedPassword = "originalPassword";
+        passwordStore.setPassword(expectedPassword);
+    }
+
+    /** @notice This test checks if other users who are not owner can set a new password. */
+    function test_anyone_can_set_password() public {
+        // Start Prank passes calls to another user -- notOwner
+        // notOwner changes the password
+        vm.startPrank(notOwner);
+        string memory changedPassword = "newPassword";
+        passwordStore.setPassword(changedPassword);
+
+        // Control is passed by back to owner to be able to call getPassword
+        vm.startPrank(owner);
+        string memory currentPassword = passwordStore.getPassword();
+
+        // This assert tests that the password was changed
+        // and that the current password is changedPassword
+        assertEq(currentPassword, changedPassword);
+    }
+}
+
+// forge test --match-path test/ --match PasswordStoreTest --match MorePasswordStoreTest  --match-test "test_anyone_can_set_password"
+// forge test --match-path test/PasswordStore.t.sol:MorePasswordStoreTest --match-test "test_anyone_can_set_password"
+// forge test --match-test "test_anyone_can_set_password"
